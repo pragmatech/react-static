0c59c57d5bb060e1627ea26b5d7cdd7ec47948b0
Darren Schmidt <darrenschmidt@Darrens-Mac-mini.local>
2018-10-28 18:39:00 -1000

    initial commit

---
+++ src/containers/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+//
+
+export default () => (
+  <div>
+    <h1>It's really about what-it-is, dig?</h1>
+    <p>
+      React, static sites, performance, speed. It's the stuff that makes us
+      tick.
+    </p>
+  </div>
+);
