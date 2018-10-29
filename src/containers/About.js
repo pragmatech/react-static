8f08cc5c4f7161396588222b82a12be4842ce9d3
Darren Schmidt <darrenschmidt@Darrens-Mac-mini.local>
2018-10-28 19:13:56 -1000

    frik

--- src/containers/About.js
+++ src/containers/About.js
@@ -3,7 +3,7 @@ import React from "react";
 
 export default () => (
   <div>
-    <h1>It's really about what-it-is, repo dat?</h1>
+    <h1>It's really about what-it-is, repo dat? Frik Me, Wally!</h1>
     <p>
       React, static sites, performance, speed. It's the stuff that makes us
       tick.
